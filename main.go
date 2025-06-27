2b8bc9d0acb8677d1687873e272d5392c346306c
Alibiderci <urazshaevalibi@gmail.com>
2025-06-27 11:19:48 +0500

    Add main.go with race-condition body

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-func main() {
+import (
+    "fmt"
+    "sync"
+)
 
+func main() {
+    var wg sync.WaitGroup
+    wg.Add(5)
+
+    for i := 0; i < 5; i++ {
+        go func() {
+            fmt.Println(i)
+            wg.Done()
+        }()
+    }
+
+    wg.Wait()
 }
 
-func MaxInt(a, c int) int {
-	if a >= c {
-		return a
-	}
+func MaxInt(a, b int) int {
+    if a >= b {
+        return a
+    }
 
-	return c
+    return b
 }
